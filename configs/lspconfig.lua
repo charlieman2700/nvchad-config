b769f9b272e05d400a7e278c8f15dc3909b32375
charlieman2700 <carlos.ace27@gmail.com>
2023-08-23 13:42:25 -0400

    Updated lsp and format options

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "svelte", "rust_analyzer", "tailwindcss", "gopls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "svelte", "rust_analyzer", "tailwindcss", "gopls", "prismals" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,25 +1,29 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "svelte" } }, -- so prettier works only on these filetypes
+	-- webdev stuff
+	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css", "svelte" } }), -- so prettier works only on these filetypes
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 
-  -- Rust
-  b.formatting.rustfmt
+	-- Rust
+	b.formatting.rustfmt,
 
+	--Prisma
+	b.formatting.prismaFmt.with({
+		command = { "go run github.com/steebchen/prisma-client-go format" },
+	}),
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
