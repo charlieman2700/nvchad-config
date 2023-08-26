8084ace273510e84fe6f693f8b219a11be853e48
Carlos Solorzano <carlos.ace27@gmail.com>
2023-08-25 23:28:19 -0600

    Added support for phpactor

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,7 +5,18 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "svelte", "rust_analyzer", "tailwindcss", "gopls", "prismals" }
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "clangd",
+  "svelte",
+  "rust_analyzer",
+  "tailwindcss",
+  "gopls",
+  "prismals",
+  "emmet_language_server",
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -14,5 +25,11 @@ for _, lsp in ipairs(servers) do
   }
 end
 
---
--- lspconfig.pyright.setup { blabla}
+lspconfig.phpactor.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    ["language_server_phpstan.enabled"] = false,
+    ["language_server_psalm.enabled"] = false,
+  },
+}
