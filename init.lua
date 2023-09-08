a7b504c5531e40b1265b8552e2c5634fa4d85a36
Carlos Solorzano <carlos.ace27@gmail.com>
2023-09-08 08:20:04 -0600

    Added Dressing plugin, line numbers on buffers

--- init.lua
+++ init.lua
@@ -10,3 +10,7 @@
 vim.wo.wrap = false
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+
+
+vim.g.number = true
--- plugins.lua
+++ plugins.lua
@@ -55,6 +55,22 @@ local plugins = {
     lazy = false,
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "stevearc/dressing.nvim",
+    event="VeryLazy",
+    opts = {},
+  },
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
