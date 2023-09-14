fd31d755442aafbfb6e5848aace48db5110ce670
Carlos Solorzano <carlos.ace27@gmail.com>
2023-09-14 12:24:41 -0600

    Aded TODO plugin

--- plugins.lua
+++ plugins.lua
@@ -67,8 +67,19 @@ local plugins = {
   },
   {
     "stevearc/dressing.nvim",
-    event="VeryLazy",
+    event = "VeryLazy",
     opts = {},
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
   },
 
   -- To make a plugin not be loaded
